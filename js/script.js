638213c346666bf2a6c3cb7d7db9cabe23da70f5
Farkhan_adhitama <mfarkhan62@gmail.com>
2023-07-13 21:20:55 +0700

    menambah produk, modal

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 // Toggle class active hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 // Ketika hamburger menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active');
+     e.preventDefault();
 }
 
 // Toggle class active search form
@@ -15,9 +16,18 @@ document.querySelector('#search-button').onclick = (e) => {
     e.preventDefault();
 }
 
+// Toggle class active shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+}
+
+
 // Klik di luar elemen
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 document.addEventListener('click', function(e){
     if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
@@ -25,6 +35,31 @@ document.addEventListener('click', function(e){
     if(!sb.contains(e.target) && !searchForm.contains(e.target)){
         searchForm.classList.remove('active');
     }
+    if(!sc.contains(e.target) && !shoppingCart.contains(e.target)){
+        shoppingCart.classList.remove('active');
+    }
+})
+
+// modal box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    }
 })
 
 
+// klik tombol close
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+// klik di luar modal
+window.onclick = (e) => {
+    if(e.target === itemDetailModal){
+        itemDetailModal.style.display = 'none';
+    }
+}
